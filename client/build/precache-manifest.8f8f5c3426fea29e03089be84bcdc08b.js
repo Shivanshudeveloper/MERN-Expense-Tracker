5aa192517a3b4279a3609769590e3eac92e841aa
Shivanshudeveloper <developer.shivanshu@gmail.com>
2020-03-26 23:13:45 +0530

    Build Added

---
+++ client/build/precache-manifest.8f8f5c3426fea29e03089be84bcdc08b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e62672e3070bcc3392ae2583293c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21e66f53de6a6b6a026",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "278af58d90f3dc496c85",
+    "url": "/static/js/2.c20a273c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c20a273c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21e66f53de6a6b6a026",
+    "url": "/static/js/main.ed8747f7.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
